21c80268ddb96409e4f457379cc35ea907018004
Richi-Kodehode <richi.boge@gmail.com>
2025-05-28 13:46:42 +0200

    more testing

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello!")
+#
+# name = input("What is your name?")
+# age = input("How old are you?")
+# age = int(age)
+# age = age+1
+#
+# print(f"Hello {name}")
+# print("Happy Birthday")
+# print(f"You are {age} old!")
+#
+#
+# length = float(input("enter length: "))
+# width = float(input("enter width: "))
+# area = length * width
+#
+# print(f"The Area is: {area}cm")
+
+# item = input("What item would you like to buy?: ")
+# price = float(input("what is the price?: "))
+# quantity = int(input("how many would you like?:"))
+# total = price * quantity
+#
+# print(f"You have bought {quantity} x {item}/s")
+# print(f"Your total is: {total}$")
+
+#Medlibs Game
+
+# adjective1 = input("Enter an adjective (description): ")
+# noun1 = input("enter a noun (person, place, thing): ")
+# adjective2 = input("Enter an adjective (description): ")
+# verb1 = input("enter a verb ending with 'ing': ")
+# adjective3 = input("enter an adjective (description): ")
+#
+# print(f"Today I whant to a {adjective1} zoo")
+# print(f"In an exhibition, I saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}")
+
